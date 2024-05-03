c241274a67e9112ab67bd5685196cac434c5a996
Trần Minh Quang <tmquang0209@gmail.com>
2024-05-03 11:00:47 +0700

    Add LoginRequest class for user login functionality

---
+++ src/main/java/com/tmquang/score/payload/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.tmquang.score.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
